922e688ff65ac1e3825c7f3348c2936932e0a659
SeaCannon1122 <coroc@buzzmail.eu>
2024-06-28 16:52:00 +0200

    initial hub

--- src/maintestnode.cpp
+++ src/maintestnode.cpp
@@ -3,71 +3,73 @@
 #include <memory>
 #include <string>
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "std_msgs/msg/bool.hpp"
 
-
-class maintestnode : public rclcpp::Node
-{
+class MaintestNode : public rclcpp::Node {
 public:
-  maintestnode() : Node("maintestnode") {
-    //cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("n10/cmd_vel", 10, std::bind(&translator::cmd_vel_callback, this, std::placeholders::_1));
-    //drive_enable_sub_ = this->create_subscription<std_msgs::msg::Bool>("n10/drive_enable", 10, std::bind(&translator::drive_enable_callback, this, std::placeholders::_1));
-    //servo_enable_sub_ = this->create_subscription<std_msgs::msg::Bool>("n10/servo_enable", 10, std::bind(&translator::servo_enable_callback, this, std::placeholders::_1));
-    //servo_cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("n10/servo_cmd_vel", 10);
-    //profmay_pub_ = this->create_publisher<??>("??", 10);
-    
+  MaintestNode() : Node("maintestnode"), publishing_(true) {
     cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/n10/cmd_vel", 10);
-    getmsgs();
+    timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(10), 
+      std::bind(&MaintestNode::Publish, this)
+    );
+    get_msgs_thread_ = std::thread(&MaintestNode::get_msgs, this);
   }
 
-    //void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
-    //  if(drive_bool) {
-    //    if(servo_bool) servo_cmd_vel_pub_->publish(*msg);
-    //  }
-    //}
-    //void drive_enable_callback(const std_msgs::msg::Bool::SharedPtr msg) {drive_bool = msg->data;}
-    //void servo_enable_callback(const std_msgs::msg::Bool::SharedPtr msg) {servo_bool = msg->data;}
-    
+  ~MaintestNode() {
+    if (get_msgs_thread_.joinable()) {
+      get_msgs_thread_.join();
+    }
+  }
 
 private:
-  //rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-  //rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr drive_enable_sub_;
-  //rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr servo_enable_sub_;
-  //rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr servo_cmd_vel_pub_;
-  //rclcpp::Publisher<??>::SharedPtr profmay_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
-
-  //bool drive_bool = true;
-  //bool servo_bool = true;
-  void getmsgs() {
-    while(rclcpp::ok()) {
-      float linearx;
-      float lineary;
-      float angular;
-      std::cout << "linear x" << '\n';
-      std::cin >> linearx;
-      std::cout << '\n' << "linear y" << '\n';
-      std::cin >> lineary;
-      std::cout << '\n' << "angular" << '\n';
-      std::cin >> angular;
-      std::cout << '\n' << "----------" << '\n';
-      auto msg = std::make_shared<geometry_msgs::msg::Twist>();
-      msg->linear.x = linearx;
-      msg->linear.y = lineary;
-      msg->angular.z = angular;
-      cmd_vel_pub_->publish(*msg);
-    }
-
+  void Publish() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto msg = geometry_msgs::msg::Twist();
+    msg.linear.x = linearx_;
+    msg.linear.y = lineary_;
+    msg.angular.z = angular_;
+    cmd_vel_pub_->publish(msg);
   }
+
+  void get_msgs() {
+    while (rclcpp::ok()) {
+      std::cout << "Enter values to publish (linear x, linear y, angular z):" << std::endl;
+      float lx;
+      float ly;
+      float a;
+      std::cout << "linear x: ";
+      std::cin >> lx;
+      std::cout << "linear y: ";
+      std::cin >> ly;
+      std::cout << "angular z: ";
+      std::cin >> a;
+
+      linearx_ = lx;
+      lineary_ = ly;
+      angular_ = a;
+
+      std::cout << "Publishing new values..." << std::endl;
+    }
+  }
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::thread get_msgs_thread_;
+  std::mutex mutex_;
+  bool publishing_;
+  float linearx_ = 0.0;
+  float lineary_ = 0.0;
+  float angular_ = 0.0;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<maintestnode>();
+  auto node = std::make_shared<MaintestNode>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
